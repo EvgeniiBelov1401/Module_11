07bbd38349ab1eaa2f108a7b338a6266c0115e0e
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-04-26 13:40:40 +0300

    сделал ядро бота

--- Module_11/VoiceTexterBot/Program.cs
+++ Module_11/VoiceTexterBot/Program.cs
@@ -1,12 +1,35 @@
-﻿using Telegram.Bot.Polling;
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Telegram.Bot;
+using VoiceTexterBot.Modules;
 
 namespace VoiceTexterBot
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static async Task Main()
         {
-            Console.WriteLine("Hello, World!");
+            Console.OutputEncoding=Encoding.Unicode;
+
+            // Объект, отвечающий за постоянный жизненный цикл приложения
+            var host = new HostBuilder().ConfigureServices((hostContext, services) => ConfigureServices(services)) // Задаем конфигурацию
+                .UseConsoleLifetime() // Позволяет поддерживать приложение активным в консоли
+                .Build(); // Собираем
+
+            Console.WriteLine("Сервис запущен");
+            // Запускаем сервис
+            await host.RunAsync();
+            Console.WriteLine("Сервис остановлен");
+        }
+        static void ConfigureServices(IServiceCollection services)
+        {
+            // Регистрируем объект TelegramBotClient c токеном подключения
+            services.AddSingleton<ITelegramBotClient>(provider => new TelegramBotClient("6777125585:AAEsltiy1v7CLEuV-rZ88x1scYojW9wps48"));
+            // Регистрируем постоянно активный сервис бота
+            services.AddHostedService<Bot>();
         }
     }
 }
