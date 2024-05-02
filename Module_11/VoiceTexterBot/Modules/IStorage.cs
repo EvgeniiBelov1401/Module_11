c07fdaaae592307e4c3e29035a4bd9e7d4e39650
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-05-02 10:28:44 +0300

    Добавил интерфейс IStorage - сервис хранения пользовательских сессий

--- Module_11/VoiceTexterBot/Modules/IStorage.cs
+++ Module_11/VoiceTexterBot/Modules/IStorage.cs
@@ -6,8 +6,7 @@ using System.Threading.Tasks;
 
 namespace VoiceTexterBot.Modules
 {
-    internal class Session
+    internal interface IStorage
     {
-        public string LanguageCode { get; set; }
     }
 }
