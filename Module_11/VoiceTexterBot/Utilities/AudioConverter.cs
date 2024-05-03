7d9fefc0f6478e5a4a4397c9ab528ba32f8ec794
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-05-03 12:57:31 +0300

    поменял уровень логирования

--- Module_11/VoiceTexterBot/Utilities/AudioConverter.cs
+++ Module_11/VoiceTexterBot/Utilities/AudioConverter.cs
@@ -12,24 +12,21 @@ namespace VoiceTexterBot.Utilities
     {
         public static void TryConvert(string inputFile, string outputFile)
         {
-            static void TryConvert(string inputFile, string outputFile)
-            {
-                GlobalFFOptions.Configure(options => options.BinaryFolder = Path.Combine(GetSolutionRoot(), "ffmpeg-win64", "bin"));
+            GlobalFFOptions.Configure(options => options.BinaryFolder = Path.Combine(GetSolutionRoot(), "ffmpeg-win64", "bin"));
 
-                FFMpegArguments
-                    .FromFileInput(inputFile)
-                    .OutputToFile(outputFile, true, options => options
-                    .WithFastStart())
-                    .ProcessSynchronously();
-            }
+            FFMpegArguments
+                .FromFileInput(inputFile)
+                .OutputToFile(outputFile, true, options => options
+                .WithFastStart())
+                .ProcessSynchronously();
+        }
 
-            static string GetSolutionRoot()
-            {
-                var dir = Path.GetDirectoryName(Directory.GetCurrentDirectory());
-                var fullname = Directory.GetParent(dir).FullName;
-                var projectRoot = fullname.Substring(0, fullname.Length - 4);
-                return Directory.GetParent(projectRoot)?.FullName;
-            }
+        private static string GetSolutionRoot()
+        {
+            var dir = Path.GetDirectoryName(Directory.GetCurrentDirectory());
+            var fullname = Directory.GetParent(dir).FullName;
+            var projectRoot = fullname.Substring(0, fullname.Length - 4);
+            return Directory.GetParent(projectRoot)?.FullName;
         }
     }
 }
